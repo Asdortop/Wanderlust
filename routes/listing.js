e14402d9238b75a62a13e5c9418a534d66cf4441
Asdortop <tejas.gvsb@gmail.com>
2025-07-28 10:04:01 +0530

    optimizing lines of code

--- routes/listing.js
+++ routes/listing.js
@@ -7,36 +7,28 @@ const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
 
+router
 //index route to show all listings
-router.get("/", wrapAsync(listingController.index));
+// create new listing route where u can enter the details of the listing
+// this will handle the form submission and save the new listing to the database
+ .route("/")
+ .get( wrapAsync(listingController.index))
+ .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)
+ );
 
-
-//new listing creating route to connect to the form
+ //new listing creating route to connect to the form
 // this will render the form to create a new listing
 router.get("/new", isLoggedIn, listingController.renderNewForm); 
 
 
-// read each listing route
-
-router.get("/:id", wrapAsync(listingController.showListing));
-
-
-// create new listing route where u can enter the details of the listing
-// this will handle the form submission and save the new listing to the database
-router.post("/",isLoggedIn, validateListing, wrapAsync(listingController.createListing)
-);
-
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put( isLoggedIn,isOwner, validateListing, wrapAsync(listingController.updateListing))
+.delete( isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
 
 //Edit Route
 
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
-
-// Update Route
-
-router.put("/:id", isLoggedIn,isOwner, validateListing, wrapAsync(listingController.updateListing));
-
-// Delete Route
-router.delete("/:id", isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
 
 module.exports = router;
